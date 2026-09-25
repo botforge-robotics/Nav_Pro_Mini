a96f5a24e7a3830b5d2b19e425f4ea4e95607247
chaitanya_mandala <nagachaitanya948@gmail.com>
2026-09-25 22:55:05 +0530

    chore(release): bump all packages, SDK, and launch manager to 2.0.0

--- navpromini_sdk/setup.py
+++ navpromini_sdk/setup.py
@@ -6,7 +6,7 @@ package_name = 'navpromini_sdk'
 
 setup(
     name=package_name,
-    version='1.0.0',
+    version='2.0.0',
     packages=[package_name, f'{package_name}.handlers'],
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
